b0b8602777d9cbf686601e35ef48377a041c71a3
teamafeia-creator <team.afeia@gmail.com>
2026-01-22 16:51:38 +0100

    Forcer ?

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
 import { Suspense, useMemo, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import Image from 'next/image';
